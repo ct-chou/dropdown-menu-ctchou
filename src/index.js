5f576b557ef7dfcd8c4f1b9ad773a9de49e6640d
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-11-22 08:51:27 -0500

    Feat: add comments for usage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import "./style.css";
+import './style.css';
+
+// button needs to have an id
+// menu needs to be the next sibling of the button
 
 class DropdownMenu {
-    constructor(buttonId) {
-        this.button = document.getElementById(buttonId);
-        this.menu = this.button.nextElementSibling;
-        
-        this.button.addEventListener("click", () => this.toggleMenu());
-        this.button.addEventListener("mouseover", () => this.toggleMenu());
-        this.menu.addEventListener("mouseleave", () => this.hideMenu());
-    }
+  constructor(buttonId) {
+    this.button = document.getElementById(buttonId);
+    this.menu = this.button.nextElementSibling;
 
-    toggleMenu() {
-        this.menu.classList.toggle("hidden");
-    }
-    
-    hideMenu() {
-        this.menu.classList.add("hidden");
-    }
+    this.button.addEventListener('click', () => this.toggleMenu());
+    this.button.addEventListener('mouseover', () => this.toggleMenu());
+    this.menu.addEventListener('mouseleave', () => this.hideMenu());
+  }
+
+  toggleMenu() {
+    this.menu.classList.toggle('hidden');
+  }
+
+  hideMenu() {
+    this.menu.classList.add('hidden');
+  }
 }
 
-const dropdown1 = new DropdownMenu("dropdown-btn");
-const dropdown2 = new DropdownMenu("dropdown-btn-2");
+const dropdown1 = new DropdownMenu('dropdown-btn');
+const dropdown2 = new DropdownMenu('dropdown-btn-2');
