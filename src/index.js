ecd351450ce7200544e69582a58b990359a5e0f2
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-11-22 08:44:25 -0500

    Feat: clean up comments

--- src/index.js
+++ src/index.js
@@ -21,28 +21,3 @@ class DropdownMenu {
 
 const dropdown1 = new DropdownMenu("dropdown-btn");
 const dropdown2 = new DropdownMenu("dropdown-btn-2");
-
-// const dropdownBtn = document.getElementById("dropdown-btn");
-// const menuItems = dropdownBtn.nextElementSibling;
-
-// function toggleMenu(menu) {
-//     menu.classList.toggle("hidden");
-// }
-
-// function hideMenu(menu) {
-//     menu.classList.add("hidden");
-// }
-
-// dropdownBtn.addEventListener("click", () => toggleMenu(menuItems));
-// dropdownBtn.addEventListener("mouseover", () => toggleMenu(menuItems)); 
-// menuItems.addEventListener("mouseleave", () => hideMenu(menuItems));
-
-// const dropdownBtn2 = document.getElementById("dropdown-btn-2");
-// const menuItems2 = dropdownBtn2.nextElementSibling;
-
-// dropdownBtn2.addEventListener("click", () => toggleMenu(menuItems2));
-// dropdownBtn2.addEventListener("mouseover", () => toggleMenu(menuItems2));
-// menuItems2.addEventListener("mouseleave", () => hideMenu(menuItems2));
-
-
-    
